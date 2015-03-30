23dcd7a4cb6a22227fe002cd187d9c8ce0afab82
axton21 <axton_n@yahoo.com>
2015-03-30 09:14:13 -0400

    done

--- lib/my_rotate.rb
+++ lib/my_rotate.rb
@@ -1,2 +1,19 @@
-# your code goes here
-# you will be monkey patching the Array class
+require 'pry'
+class Array
+	def my_rotate(num=1)
+		result = Array.new(self)
+		if num >=1
+			num.times do
+				shifted = result.shift
+				result.push(shifted)
+			end
+		else
+			(num*-1).times do
+				popped = result.pop
+				result.unshift(popped)
+			end
+		end
+		result.compact
+	end
+
+end
